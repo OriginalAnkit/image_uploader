2ffbe893fb1225ffb4058eed96d58227b3f905a8
ankit <akt.halder@gmail.com>
2020-03-22 11:08:49 +0530

    validation added

--- public/main.js
+++ public/main.js
@@ -1,20 +1,25 @@
 $(
     function () {
         let form = $("form");
+        let allowedFormats = ["jpg", "jpeg", "png", "gif"]
         form.submit(function (e) {
             e.preventDefault();
             let formData = $(e.target);
-            let data = new FormData()
+            let selectedFile = formData.find("input[type='file']")[0].files[0].name.split(".").pop();
+            if (!allowedFormats.find(f => f == selectedFile)) {
+                alert(`only ${allowedFormats} are allowed`)
+                return
+            }
+            let data = new FormData();
             data.append("name", formData.find("input[type='text']").val())
-            // data.append("uploadType", )
-            data.append("file",formData.find("input[type='file']")[0].files[0])
+            data.append("file", formData.find("input[type='file']")[0].files[0])
             $.ajax({
-                url: '/upload/'+formData.find("select").val(),
+                url: '/upload/' + formData.find("select").val(),
                 data: data,
                 contentType: false,
                 processData: false,
                 type: 'POST',
-                success: function(data){
+                success: function (data) {
                     getFiles()
                 }
             });
@@ -26,17 +31,18 @@ $(
 )
 
 
-getFiles=function(){
+getFiles = function () {
     $(".uploaded-sec").empty()
     $.ajax({
         url: '/getfiles',
         type: 'GET',
-        success: function({data}){
+        success: function ({ data }) {
             data.forEach(
-            d=>{
-                let tempname=d.split("/");
-                $(".uploaded-sec").append(`<div class="col-4"><img style="width:100% " src="${d}"/><h5>${tempname[tempname.length-1]}</h5></div>`)}
-            
+                d => {
+                    let tempname = d.split("/");
+                    $(".uploaded-sec").append(`<div class="col-4"><img style="width:100% " src="${d}"/><h5>${tempname[tempname.length - 1]}</h5></div>`)
+                }
+
             )
         }
     });
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -4,12 +4,11 @@ const fs = require('fs');
 
 const uploadDir = path.join(__dirname, '../public/uploads')
 
-const storage = multer.diskStorage({
+const storageLocal = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, uploadDir)
     },
     filename: function (req, file, cb) {
-        console.log(req.body, file)
         let { originalname } = file;
         let tempName = originalname.split(".")
         cb(null, (req.body.name || Date.now()) + "." + tempName[tempName.length - 1]);
@@ -22,7 +21,7 @@ function postFile(req, res) {
     let type = req.params.type;
     switch (type) {
         case "Local":
-            const upload = multer({ storage: storage })
+            const upload = multer({ storage: storageLocal })
             upload.single("file")(req, res, (e, d) => {
                 if (!e) {
                     res.json({
